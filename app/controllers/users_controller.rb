8dae1432b97b3f92eb48319917280cda94dda7a9
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-05-22 00:21:47 +1000

    sign up form WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     # load_and_authorize_resource
 
     def show
+
         @user = User.find(current_user.id)
         # p @user.pet_sitter_services.joins(:service)
         @user.pet_sitter_services.each do |service|
@@ -11,8 +12,14 @@ class UsersController < ApplicationController
     end
 
     def delete
+
         @user.destroy
         flash[:alert] = 'Successfully deleted!'
         redirect_to root_path
     end
+
+    # def user_params
+
+    #     params.require(:user).permit(:role_id)
+    # end
 end
