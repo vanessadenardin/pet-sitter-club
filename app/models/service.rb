e587cde2b0e0bed77965d8440e5242f09c7b449a
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-05-22 21:11:30 +1000

    adding validations WIP

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,6 @@ class Service < ApplicationRecord
   has_many :users, through: :pet_sitter_services, foreign_key: 'pet_sitter_id'
 
   validates :name, presence: true, length: { minimum: 2 }
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: true
   validates :description, length: { is: 5 }, allow_blank: true
 end
